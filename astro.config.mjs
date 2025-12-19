c3246fa2990815fcb1f20e114f5918faa7c4ffe2
HaydenEquityAi <hayden@hhpasset.com>
2025-12-19 00:56:22 -0600

    Set up Tailwind for production

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'static',
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [tailwind()]
 });
 
